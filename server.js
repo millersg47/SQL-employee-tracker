d8bed2d8c5964578633d49e9fccffe5090be041e
Sophie Miller <millersg47@gmail.com>
2022-05-10 20:17:13 -0700

    adds inquirer prompt function for initial question

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
-const 
+const inquirer = require("inquirer");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -25,6 +25,19 @@ app.use('/departments', depts);
 app.use('/roles', roles);
 app.use('/employees', emps);
 
+function firstQ(){
+  inquirer.prompt([
+    {
+      type: 'firstQuest',
+      message: 'What do you want to do?',
+      choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee'],
+      type: 'list'
+    }
+  ])
+  .then(answers => {
+    console.log('Answer:', answers.firstQuest)
+  })
+}; 
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
